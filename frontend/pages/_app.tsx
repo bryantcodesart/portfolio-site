8816ad23197ac312ba048cab020d91f783315423
barefootfunk <bryant@barefootfunk.com>
2022-09-14 21:59:59 -0500

    Fix typo

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import React, from 'react';
+import React from 'react';
 import { DefaultSeo } from 'next-seo';
 import { useTimeout } from 'usehooks-ts';
 import { CustomCursor } from '../src/CustomCursor';
