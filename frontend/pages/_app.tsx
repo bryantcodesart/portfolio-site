dae2d05a73ce768df51974becbfb9006190ec84c
barefootfunk <bryant@barefootfunk.com>
2022-09-14 21:59:24 -0500

    Fix lint

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import React, { useEffect } from 'react';
+import React, from 'react';
 import { DefaultSeo } from 'next-seo';
 import { useTimeout } from 'usehooks-ts';
 import { CustomCursor } from '../src/CustomCursor';
