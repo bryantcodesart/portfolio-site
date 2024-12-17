7d68886997e21e22862fb5d64c4998856bb43427
bryantcodesart <bryant@bryantcodes.art>
2024-12-16 23:30:43 -0600

    Update about content

--- frontend/src/aboutContent.tsx
+++ frontend/src/aboutContent.tsx
@@ -4,35 +4,42 @@ import jpImage from '../public/images/jp.jpg';
 /* eslint-disable no-multiple-empty-lines */
 /* eslint-disable max-len */
 export const aboutContent = {
-  intro: ['I\'m Bryant! (he/him)', 'I build web experiences'],
+  intro: ["I'm Bryant! (he/him)", 'I build web experiences'],
   mission: [
     'I am a freelance dev who helps awesome designers (like you) build their wildest dreams.',
     'Together, let\'s create something that stands out––and has your users saying, "woah."',
   ],
-  testimonials: [{
-    quote: 'Bryant\'s kickass work is complemented by his infectious energy and passion for creating original, exciting work. He is a true creative partner––always bringing new ideas to the table.',
-    shortName: 'Stef',
-    name: 'Stephanie Jung',
-    headshot: stefImage,
-    title: [
-      'Brand Design Lead, Employer Marketing at Handshake',
-    ],
-  }, {
-    quote: 'Bryant\'s collaborative mindset and aptitude to explore ideas well beyond the minimum viable product make him an invaluable partner.',
-    name: 'JP Ramirez',
-    shortName: 'JP',
-    title: ['Design Manager, Brand Design Strategy at Intuit',
-      'Director, Studio Ramírez'],
-    headshot: jpImage,
-  }, {
-    quote: 'Bryant\'s building enables award-winning projects––including CSSDA Special Kudos, CSSDA Best UI/UX/Innovation, 4x STA100, Type Director\'s Club, Webby, Webby For Good... & more.',
-    name: 'The award people',
-    shortName: 'Awards',
-    headshot: '/images/star-icon.svg',
-  }],
+  testimonials: [
+    {
+      quote:
+        "Bryant's kickass work is complemented by his infectious energy and passion for creating original, exciting work. He is a true creative partner––always bringing new ideas to the table.",
+      shortName: 'Stef',
+      name: 'Stephanie Jung',
+      headshot: stefImage,
+      title: ['Brand Design Lead, Employer Marketing at Handshake'],
+    },
+    {
+      quote:
+        "Bryant's collaborative mindset and aptitude to explore ideas well beyond the minimum viable product make him an invaluable partner.",
+      name: 'JP Ramirez',
+      shortName: 'JP',
+      title: [
+        'Design Manager, Brand Design Strategy at Intuit',
+        'Director, Studio Ramírez',
+      ],
+      headshot: jpImage,
+    },
+    {
+      quote:
+        "Bryant's building enables award-winning projects––including FWA Site Of The Day, Webby, 2x Awwwards Honorable Mention, CSSDA Special Kudos, CSSDA Best UI/UX/Innovation, 4x STA100, Type Director's Club, Webby For Good... & more.",
+      name: 'The award people',
+      shortName: 'Awards',
+      headshot: '/images/star-icon.svg',
+    },
+  ],
   skills: [
     `
-      7 years working with
+      9 years working with
       best-in-class designers building award-winning projects
     `,
     `
